7ffca5d396fae78590c28501f9138b8cd10d349f
jinhoh21 <jinhoh21@gmail.com>
2022-12-22 08:21:12 +0900

    add controller

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+      log.info("hi, this is demo");
+
+      return "hi, this is string demo";
+    }
+
+}
